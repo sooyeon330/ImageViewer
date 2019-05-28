3f7cedb66b8cbd4ac2fbbb5fe9871c9c032a9720
Sooyeon Chae <cotndus3@naver.com>
2019-05-28 11:06:36 +0900

    #2 이전버튼, 다음버튼 구현

--- ImageViewer/Form1.cs
+++ ImageViewer/Form1.cs
@@ -12,9 +12,43 @@ namespace ImageViewer
 {
     public partial class Form1 : Form
     {
+        int imgCount=0;//현재보고있는 이미지 번호
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.picImg.Image = this.imgList.Images[imgCount];
+            this.lblName.Text = (this.imgCount + 1) + "/" + this.imgList.Images.Count;
+        }
+
+        private void BtnPrev_Click(object sender, EventArgs e)
+        {
+            this.imgCount--;
+            if (imgCount < 0)
+            {
+                this.imgCount = this.imgList.Images.Count - 1;
+            }
+
+            Show();
+        }
+
+        private void Show()
+        {
+            this.picImg.Image = this.imgList.Images[this.imgCount];
+            this.lblName.Text = (this.imgCount + 1) + "/" + this.imgList.Images.Count;
+        }
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            this.imgCount++;
+            if (imgCount > this.imgList.Images.Count-1)
+            {
+                this.imgCount = 0;
+            }
+            Show();
         }
     }
 }
